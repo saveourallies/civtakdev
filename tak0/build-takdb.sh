522af0860b573e1e5e4d5817969ddec12de0c6ea
Kris Thompson <kris@saveourallies.org>
2024-12-03 09:46:43 +0000

    cleanup

--- tak0/build-takdb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker build \
-    -t takserver-db:"$(cat server/tak/version.txt)" \
-    -f server/docker/Dockerfile.takserver-db \
-    server
--- tak0/build-takserver.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker build \
-    -t takserver:"$(cat server/tak/version.txt)" \
-    -f server/docker/Dockerfile.takserver \
-    server
